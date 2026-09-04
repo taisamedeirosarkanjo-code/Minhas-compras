066e5fc930c930e05f5d413410477bea669994bd
TAÍSA MEDEIROS <taisa.medeiros.arkanjo@gmail.com>
2026-09-04 15:57:44 -0300

    Adiciona classe SQLiteDatabaseHelper

--- Helpers/SQLiteDatabaseHelper.cs
+++ Helpers/SQLiteDatabaseHelper.cs
@@ -21,7 +21,6 @@ namespace MauiAppMinhasCompras.Helpers
         public Task<List<Produto>> Update(Produto p)
         {
             string sql = "UPDATE Produto SET Descricao=?, Quantidade=?, Preco=? WHERE Id=?";
-
             return _conn.QueryAsync<Produto>(
                 sql, p.Descricao, p.Quantidade, p.Preco, p.Id
             );
@@ -40,7 +39,6 @@ namespace MauiAppMinhasCompras.Helpers
         public Task<List<Produto>> Search(string q)
         {
             string sql = "SELECT * FROM Produto WHERE Descricao LIKE '%" + q + "%'";
-
             return _conn.QueryAsync<Produto>(sql);
         }
     }
