068e2914ab292960dd2709f1fdf06cf8309e7812
TAÍSA MEDEIROS <taisa.medeiros.arkanjo@gmail.com>
2026-09-11 18:07:27 -0300

    Agenda 6: EditarProduto.xaml.cs preservando a Categoria ao atualizar

---
+++ Agenda06/Views/EditarProduto.xaml.cs
@@ -0,0 +1,36 @@
+using MauiAppMinhasCompras.Models;
+
+namespace MauiAppMinhasCompras.Views;
+
+public partial class EditarProduto : ContentPage
+{
+    public EditarProduto()
+    {
+        InitializeComponent();
+    }
+
+    private async void ToolbarItem_Clicked(object sender, EventArgs e)
+    {
+        try
+        {
+            Produto produto_anexado = BindingContext as Produto;
+
+            Produto p = new Produto
+            {
+                Id = produto_anexado.Id,
+                Descricao = txt_descricao.Text,
+                Categoria = cmb_categoria.SelectedItem as string ?? produto_anexado.Categoria,
+                Quantidade = Convert.ToDouble(txt_quantidade.Text),
+                Preco = Convert.ToDouble(txt_preco.Text)
+            };
+
+            await App.Db.Update(p);
+            await DisplayAlert("Sucesso!", "Registro Atualizado", "OK");
+            await Navigation.PopAsync();
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Ops", ex.Message, "OK");
+        }
+    }
+}
